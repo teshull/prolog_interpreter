f2833e89b8e5ba8db5bb5dca2fb24711fe10cb28
Tom Shull <mail@tomshull.com>
2020-07-10 22:21:48 -0500

    forgot to add this previously

---
+++ src/main/java/cs598ga/shull/prolog/runtime/Log.java
@@ -0,0 +1,27 @@
+package cs598ga.shull.prolog.runtime;
+
+import cs598ga.shull.prolog.nodes.BaseNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Log {
+    public static boolean allPhases = false;
+    public static Set<Phase> enabledPhases = new HashSet<>();
+    public enum Phase {
+        ALL,
+        FACT,
+        RULE,
+        PARSING,
+        CUT,
+    }
+
+    public static void logMessage(Phase phase, String message){
+        if(allPhases || enabledPhases.contains(phase)){
+            System.out.println(message);
+        }
+    }
+    public static void logMessage(Phase phase, BaseNode node){
+        logMessage(phase, node.toString());
+    }
+}
