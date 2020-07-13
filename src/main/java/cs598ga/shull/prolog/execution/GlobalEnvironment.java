b4aba185438474e9012fe3c293ebe1dd9f0b2bb2
Tom Shull <mail@tomshull.com>
2020-07-12 20:51:16 -0500

    cleaning up global environment

--- src/main/java/cs598ga/shull/prolog/execution/GlobalEnvironment.java
+++ src/main/java/cs598ga/shull/prolog/execution/GlobalEnvironment.java
@@ -10,46 +10,30 @@ import cs598ga.shull.prolog.nodes.builtin.BuiltinNode;
 public class GlobalEnvironment {
 	final public static GlobalEnvironment globalEnv = new GlobalEnvironment();
 
-	Map<String, ArrayList<PredicateNode>> predicates;
-	Map<String, ArrayList<PredicateNode>> builtins;
+	Map<String, ArrayList<PredicateNode>> world;
 	public ArrayList<QueryNode> queries;
 	static final ArrayList<PredicateNode> NORESULT = new ArrayList<>();
 	
 	public GlobalEnvironment(){
-		predicates = new HashMap<>();
-		builtins = new HashMap<>();
+		world = new HashMap<>();
 		queries = new ArrayList<>();
 	}
 	
-	public void initializePredIfEmpty(String name){
-		if(!predicates.containsKey(name)){
-			predicates.put(name, new ArrayList<PredicateNode>());
-		}
-	}
-
-	public void initializeBuiltinIfEmpty(String name){
-		if(!builtins.containsKey(name)){
-			builtins.put(name, new ArrayList<PredicateNode>());
+	public void initializeWorldIfEmpty(String name){
+		if(!world.containsKey(name)){
+			world.put(name, new ArrayList<PredicateNode>());
 		}
 	}
 
 	public void addRuleNode(RuleNode node){
 		String name = node.getName();
-		initializePredIfEmpty(name);
-		ArrayList<PredicateNode> clauses = predicates.get(name);
+		initializeWorldIfEmpty(name);
+		ArrayList<PredicateNode> clauses = world.get(name);
 		clauses.add(node);
 	}
 	
 	public ArrayList<PredicateNode> getPredicates(String s){
-		ArrayList<PredicateNode> result = predicates.get(s);
-		ArrayList<PredicateNode> resultB = builtins.get(s);
-		if(resultB != null){
-			if(result == null){
-				result = resultB;
-			} else {
-				result.addAll(resultB);
-			}
-		}
+		ArrayList<PredicateNode> result = world.get(s);
 		if(result == null){
 			return NORESULT;
 		}
@@ -59,8 +43,8 @@ public class GlobalEnvironment {
 
 	public void addFactNode(FactNode node){
 		String name = node.getName();
-		initializePredIfEmpty(name);
-		ArrayList<PredicateNode> clauses = predicates.get(name);
+		initializeWorldIfEmpty(name);
+		ArrayList<PredicateNode> clauses = world.get(name);
 		clauses.add(node);
 	}
 	
@@ -70,15 +54,15 @@ public class GlobalEnvironment {
 
 	public void addBuiltinNode(BuiltinNode node){
 		String name = node.getName();
-		initializeBuiltinIfEmpty(name);
-		ArrayList<PredicateNode> clauses = builtins.get(name);
+		initializeWorldIfEmpty(name);
+		ArrayList<PredicateNode> clauses = world.get(name);
 		clauses.add(node);
 	}
 	
 	
 	public void printEnvironment(){
-		System.out.println("predicates");
-		for(ArrayList<PredicateNode> clauses : predicates.values()){
+		System.out.println("world");
+		for(ArrayList<PredicateNode> clauses : world.values()){
 			for(ClauseNode clause : clauses){
 				System.out.println(clause);
 			}
